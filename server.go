146268e26186633def3c6b9bab3fd5adf9d274f4
L. Gabriel Miller <gmiller2007@gmail.com>
2013-11-12 19:56:14 -0500

    Makes serving static files work as intended. Weeds out unused code and modules.

--- server.go
+++ server.go
@@ -4,7 +4,6 @@ import(
     "fmt"
     "net/http"
     "html/template"
-    "log"
 )
 
 
@@ -23,27 +22,26 @@ func serveFile(pattern string, filename string) {
     })
 }
 
-func Log(handler http.Handler) http.Handler {
+func printOutput(handler http.Handler) http.Handler {
     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL)
+        fmt.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
     handler.ServeHTTP(w, r)
     })
 }
 
 func main(){
     fmt.Println(" > HTTP Server running...")
-    http.Handle("/static/", http.FileServer(http.Dir("./static/"))) // y u no werk
+
+    http.HandleFunc("/static/", func(w http.ResponseWriter, r *http.Request) {
+        http.ServeFile(w, r, r.URL.Path[1:])
+    })
+
     http.HandleFunc("/", root)
     //http.HandleFunc("/item", item)
     //http.Handle("/static/", http.FileServer(http.Dir("./static")))
 
-    serveFile("/sitemap.xml", "./sitemap.xml")
     serveFile("/favicon.ico", "./favicon.ico")
     serveFile("/robots.txt", "./robots.txt")
 
-    http.ListenAndServe(":1234", Log(http.DefaultServeMux))
-    //err := http.ListenAndServe(":1234", nil)
-    //if err != nil {
-    //    log.Fatal("ListenAndServe: ", err)
-    //}
+    http.ListenAndServe(":1234", printOutput(http.DefaultServeMux))
 }
